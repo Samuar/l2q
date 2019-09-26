7b466df80f79244bbeec76b3af35c2deffa05e79
Sam Holland <sam.h@xewli.com>
2019-09-26 13:58:23 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='l2q',
+    keywords='query builder',
+    version='0.1',
+    author='Sam Holland',
+    author_email='sam.h@xewli.com',
+    url='http://github.com/samuar/l2q',
+    license='GPLv3+',
+    platforms=['linux', 'mac'],
+    packages=['l2q'],
+    install_requires=[
+        'numpy'
+    ],
+    entry_points={
+        'console_scripts': [
+            'l2q = l2q.__main__:main'
+        ]
+    },
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+    ],
+)
