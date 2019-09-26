97fdd6687fddd91ffa9fb793f1712f78d38b1bf2
Sam Holland <sam.h@xewli.com>
2019-09-26 14:36:15 +0100

    Changed platform, added all requirements to install_requirements

--- setup.py
+++ setup.py
@@ -3,16 +3,17 @@ from setuptools import setup
 setup(
     name='l2q',
     keywords='query builder',
-    version='1.1',
+    version='1.2',
     author='Sam Holland',
     author_email='sam.h@xewli.com',
+    descriptiom='Concatenates a list of terms into a single boolean OR query',
     url='https://github.com/Samuar/l2q',
     license='GPLv3+',
-    platforms=['linux', 'mac'],
+    platforms=['any'],
     packages=['l2q'],
-    download_url='https://github.com/Samuar/l2q/archive/v1.1.tar.gz',
+    download_url='https://github.com/Samuar/l2q/archive/v1.2.tar.gz',
     install_requires=[
-        'numpy'
+        'pandas', 'xlrd', 'python-docx'
     ],
     entry_points={
         'console_scripts': [
